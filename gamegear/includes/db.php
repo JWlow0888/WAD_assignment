31a44bb67bdb649edfb1918cfdb3ba245983edea
Low <lowjunwei69@gmail.com>
2026-08-23 16:53:40 +0800

    Set database connection character set to utf8mb4
    Set the character set for the database connection

--- gamegear/includes/db.php
+++ gamegear/includes/db.php
@@ -1,6 +1,4 @@
 <?php
-// Shared database connection
-// Edit these if your WAMP MySQL setup uses a different username/password
 $db_host = "localhost";
 $db_user = "root";
 $db_pass = "";
@@ -11,4 +9,6 @@ $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 if (!$conn) {
     die("Database connection failed: " . mysqli_connect_error());
 }
+
+mysqli_set_charset($conn, "utf8mb4");
 ?>
